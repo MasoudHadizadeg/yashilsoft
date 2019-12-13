c90f4191bcc895fabb8f895a920cf58092f10f24
Masoud Hadizadeh <g.hadizadeh.masoud@gmail.com>
2019-12-13 09:54:10 +0330

    Angular 8

--- YashilSoft/Yashil.WebHost/ClientApp/src/app/shared/routes/full-split-layout.routs.ts
+++
@@ -1,9 +0,0 @@
-import {Routes, RouterModule} from '@angular/router';
-
-// Route for content layout with sidebar, navbar and footer
-export const Full_SPLIT_ROUTES: Routes = [
-    {
-        path: '',
-        loadChildren: () => import('./pages/full-layout-split-pages/full-split-pages.module').then(m => m.FullSplitPagesModule)
-    }
-];
